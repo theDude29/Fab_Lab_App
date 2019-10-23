8106df65c24725d4f7ccda166e26ea2c10a48b6e
the_Dude <remi.perenne@free.fr>
2019-10-23 09:52:20 +0200

    page presentation

--- SRC/App.js
+++ SRC/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Presentation from './Vue/Components/Presentation'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Aplli du fab lab !!!!!!!!!!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+    render() {
+        return (
+            <Presentation />
+        )
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ SRC/Vue/Components/Presentation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export default class Presentation extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>App du fab lab</Text>
+            </View>
+        )
+    }
+}
+
+export default Presentation
