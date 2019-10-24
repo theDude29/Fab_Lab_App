85a404be3913c13ba5310d52e202ea0198ecb01d
the_Dude <remi.perenne@free.fr>
2019-10-24 11:34:18 +0200

    drawer navigator

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import Navigation from './SRC/Vue/Navigation/Navigation'
 export default class App extends React.Component {
     render() {
         return (
-            <Navigation />
+            <Navigation/>
         )
     }
 }
