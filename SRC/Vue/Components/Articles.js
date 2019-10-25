491c3390abbdb2ce811bc9b95acefe2dedf63e3a
the_Dude <remi.perenne@free.fr>
2019-10-25 17:55:03 +0200

    je commence la mise en page

--- SRC/Vue/Components/Articles.js
+++ SRC/Vue/Components/Articles.js
@@ -1,14 +1,36 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, ImageBackground, StyleSheet} from 'react-native'
 
 class Articles extends React.Component {
     render() {
         return (
-            <View>
-                <Text>Articles</Text>
-            </View>
+            <ImageBackground
+                style={styles.image}
+                source={require('../ressources/images/articles.jpg')}
+            >
+                <View style={styles.main_container}>
+                    <Text style={styles.default_text}>Article singe\n singe!</Text>
+                </View>
+            </ImageBackground>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    default_text: {
+        color: "white",
+        fontSize: 30
+    },
+    main_container: {
+        margin: 20,
+        padding: 15,
+        backgroundColor: 'rgba(100,100,100,0.5)',
+        borderRadius: 20,
+    }
+})
 
 export default Articles
--- SRC/Vue/Components/Presentation.js
+++ SRC/Vue/Components/Presentation.js
@@ -1,16 +1,38 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet, ImageBackground} from 'react-native'
 
 class Presentation extends React.Component {
     render() {
         return (
-            <View>
-                <Text>Bienvenu dans l'application du fab lab !</Text>
-                <Text>texte de prsentation qsdfdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</Text>
-                <Text>Dernier publication : sqdfffffffdddddddddddddddddddddddddd</Text>
-            </View>
+            <ImageBackground
+                style={styles.image}
+                source={require('../ressources/images/presentation.jpg')}
+            >
+                <View style={styles.main_container}>
+                    <Text style={styles.default_text}>Bienvenu dans l'application du fab lab !</Text>
+                    <Text style={styles.default_text}>texte de prsentation qsdfdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</Text>
+                    <Text style={styles.default_text}>Dernier publication : sqdfffffffdddddddddddddddddddddddddd</Text>
+                </View>
+            </ImageBackground>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    default_text: {
+        color: "white",
+        fontSize: 30
+    },
+    main_container: {
+        margin: 20,
+        padding: 15,
+        backgroundColor: 'rgba(100,100,100,0.5)',
+        borderRadius: 20,
+    }
+})
 
 export default Presentation
